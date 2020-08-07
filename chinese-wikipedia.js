c039336ee5eec1d74745929673af5655560079f3
Song <quanpengsong@gmail.com>
2020-08-07 16:32:11 +0800

    :tada: add new script

---
+++ chinese-wikipedia.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         维基百科繁体自动切简体
+// @namespace    https://iamazing.cn/
+// @version      0.1
+// @description  自动跳转
+// @author       JustSong
+// @match        https://zh.wikipedia.org/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let pathname = window.location.pathname;
+    if (!pathname.startsWith("/zh-cn/")) window.location.pathname = pathname.replace("wiki", "zh-cn");
+})();
